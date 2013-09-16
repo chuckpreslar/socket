2aa6ac4c1b4251757728b9de5e18c02f8e88dff4
Chuck Preslar <chuckpreslar@gmail.com>
2013-09-15 20:56:20 -0400

    Initial events and types.

--- socket.go
+++ socket.go
@@ -1,1 +1,31 @@
 package socket
+
+type Event uint8
+
+const (
+  CONNECT Event = iota
+  CONNECTION
+  CLOSE
+  DATA
+  DRAIN
+  END
+  ERROR
+  LISTENING
+  TIMEOUT
+)
+
+type Server struct{}
+type Socket struct{}
+type Buffer struct{}
+
+type Handler interface {
+  handler()
+}
+
+type EventHandler func() error
+type ConnectionHandler func(Socket) error
+type DataHandler func(Buffer) error
+
+func (h EventHandler) handler()      {}
+func (h ConnectionHandler) handler() {}
+func (h DataHandler) handler()       {}
