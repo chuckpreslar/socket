afdcb46600905936308a0cbe78bc4439309d0202
Chuck Preslar <chuckpreslar@gmail.com>
2013-09-16 17:06:51 -0400

    Initial structure prototypes.

--- socket.go
+++ socket.go
@@ -3,8 +3,7 @@ package socket
 type Event uint8
 
 const (
-  CONNECT Event = iota
-  CONNECTION
+  CONNECTION Event = iota
   CLOSE
   DATA
   DRAIN
@@ -14,8 +13,6 @@ const (
   TIMEOUT
 )
 
-type Server struct{}
-type Socket struct{}
 type Buffer struct{}
 
 type Handler interface {
@@ -29,3 +26,23 @@ type DataHandler func(Buffer) error
 func (h EventHandler) handler()      {}
 func (h ConnectionHandler) handler() {}
 func (h DataHandler) handler()       {}
+
+type Emitter struct {
+  events map[Event][]Handler
+}
+
+func (e *Emitter) AddListener(event Event, handler Handler) {}
+func (e *Emitter) On(event Event, handler Handler)          {}
+func (e *Emitter) RemoveListener(handler Handler)           {}
+func (e *Emitter) Off(handler Handler)                      {}
+
+type Server struct {
+  *Emitter
+}
+
+func (s *Server) Listen() (err error) {}
+func (s *Server) Close() (err error)  {}
+
+type Socket struct {
+  *Emitter
+}
